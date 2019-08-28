883e745b09979d124cdcdfb7e76e91e3cc5356bb
Erandir <aefs12junior@gmail.com>
2019-08-28 20:28:32 -0300

    const and let

---
+++ cap 6/index.js
@@ -0,0 +1,19 @@
+const name = 'Erandir'
+
+console.log(name)
+
+const people = {
+	name: 'Junior'
+}
+
+people.age = 23
+
+console.log(people)
+
+let number = 1
+
+console.log(number)
+
+number = 2
+
+console.log(number)
